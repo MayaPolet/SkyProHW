fef99ffa906a08a2d1e023a6fa88c783738896c3
Maya Poletaeva <mfp.ru>
2024-05-24 17:19:35 +0300

    в test_processing добавлен @pytest.fixture

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,36 +1,25 @@
-# import pytest
+import pytest
 from src.processing import only_state, sorted_date
 
 
-# @pytest.fixture
-# def info_list_for_test() -> str:
-#      return  [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-#     !!! ЕСЛИ ТАК ПИШУ, ТО ГОВОРИТ
-#     TypeError: 'function' object is not iterable
-#     не знаю как это поправить = > вынесла тестовый список в начало модуля
+@pytest.fixture
+def info_list_for_test():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-info_list_for_test = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
-def test_only_state():
+def test_only_state(info_list_for_test):
     assert only_state(info_list_for_test) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
 
-def test_sorted_date():
+def test_sorted_date(info_list_for_test):
     assert sorted_date(info_list_for_test) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
